8a8b213a57edd02662360596a805a4a88ee3d03c
Pierre-Yves Monnet <pierre-yves.monnet@bonitasoft.com>
2015-11-19 12:17:44 -0800

    First version

---
+++ custompage_SDEdashboard/Index.groovy
@@ -0,0 +1,206 @@
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+import java.text.SimpleDateFormat;
+import java.util.logging.Logger;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.lang.Runtime;
+
+import org.json.simple.JSONObject;
+import org.codehaus.groovy.tools.shell.CommandAlias;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONValue;
+
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.sql.DataSource;
+import java.sql.DatabaseMetaData;
+
+import org.apache.commons.lang3.StringEscapeUtils
+
+import org.bonitasoft.engine.identity.User;
+import org.bonitasoft.console.common.server.page.PageContext
+import org.bonitasoft.console.common.server.page.PageController
+import org.bonitasoft.console.common.server.page.PageResourceProvider
+import org.bonitasoft.engine.exception.AlreadyExistsException;
+import org.bonitasoft.engine.exception.BonitaHomeNotSetException;
+import org.bonitasoft.engine.exception.CreationException;
+import org.bonitasoft.engine.exception.DeletionException;
+import org.bonitasoft.engine.exception.ServerAPIException;
+import org.bonitasoft.engine.exception.UnknownAPITypeException;
+
+import com.bonitasoft.engine.api.TenantAPIAccessor;
+import org.bonitasoft.engine.session.APISession;
+import org.bonitasoft.engine.api.CommandAPI;
+import org.bonitasoft.engine.api.ProcessAPI;
+import org.bonitasoft.engine.api.ProfileAPI;
+import org.bonitasoft.engine.api.IdentityAPI;
+import com.bonitasoft.engine.api.PlatformMonitoringAPI;
+import org.bonitasoft.engine.search.SearchOptionsBuilder;
+import org.bonitasoft.engine.search.SearchResult;
+import org.bonitasoft.engine.bpm.flownode.ActivityInstanceSearchDescriptor;
+import org.bonitasoft.engine.bpm.flownode.ArchivedActivityInstanceSearchDescriptor;
+import org.bonitasoft.engine.bpm.flownode.ActivityInstance;
+import org.bonitasoft.engine.bpm.flownode.ArchivedFlowNodeInstance;
+import org.bonitasoft.engine.bpm.flownode.ArchivedActivityInstance;
+import org.bonitasoft.engine.search.SearchOptions;
+import org.bonitasoft.engine.search.SearchResult;
+
+import org.bonitasoft.engine.command.CommandDescriptor;
+import org.bonitasoft.engine.command.CommandCriterion;
+import org.bonitasoft.engine.bpm.flownode.ActivityInstance;
+import org.bonitasoft.engine.bpm.process.ProcessDeploymentInfo;
+	
+import com.santos.sdeaccess.SdeAccess;
+import com.santos.sdeaccess.SdeAccess.ListCasesParameter;
+import com.santos.sdeaccess.SdeBusinessAccess.SystemSummaryParameter;
+import com.santos.sdeaccess.SdeBusinessAccess.WellListParameter;
+import com.santos.sdeaccess.SdeBusinessAccess.CreateWellParameter;
+
+import com.santos.sdeaccess.SdeAccess.StartprocessParameter;
+  
+public class Index implements PageController {
+
+	@Override
+	public void doGet(HttpServletRequest request, HttpServletResponse response, PageResourceProvider pageResourceProvider, PageContext pageContext) {
+	
+		Logger logger= Logger.getLogger("org.bonitasoft");
+		HashMap<String,Object> result=null;
+		PrintWriter out = response.getWriter()
+
+		
+		try {
+			def String indexContent;
+			pageResourceProvider.getResourceAsStream("Index.groovy").withStream { InputStream s-> indexContent = s.getText() };
+			response.setCharacterEncoding("UTF-8");
+
+			String action=request.getParameter("action");
+			logger.info("###################################### action is["+action+"] V4!");
+			if (action==null || action.length()==0 )
+			{
+				logger.severe("RUN Default !");
+				
+				runTheBonitaIndexDoGet( request, response,pageResourceProvider,pageContext);
+				return;
+			}
+			
+			APISession session = pageContext.getApiSession()
+			ProcessAPI processAPI = TenantAPIAccessor.getProcessAPI(session);
+			ProfileAPI profileAPI = TenantAPIAccessor.getProfileAPI(session);
+			PlatformMonitoringAPI platformMonitoringAPI = TenantAPIAccessor.getPlatformMonitoringAPI(session);
+			IdentityAPI identityAPI = TenantAPIAccessor.getIdentityAPI(session);
+			
+			if ("getWellTrackerDashboardList".equals(action))
+			{
+				String json= request.getParameter("json");
+				logger.info("SdeDashBoard.groovy: ParamJson ="+json);
+				ListCasesParameter listCasesParameter = ListCasesParameter.getFromJson(json);
+				result = SdeAccess.getWellTrackerDashboardList(listCasesParameter, session, processAPI, 
+				identityAPI );
+				logger.info("getListCasesForSdeDashboard:Result="+result.toString());
+			}	
+			else if ("getWellList".equals(action))
+			{
+				String json= request.getParameter("json");
+				logger.info("SdeDashBoard.groovy: ParamJson ="+json);
+				WellListParameter wellListParameter = WellListParameter.getFromJson(json);
+				result = SdeAccess.getWellList(wellListParameter);
+				logger.info("getWellList:Result="+result.toString());
+			}	
+			else if ("createWellList".equals(action))
+			{
+				String json= request.getParameter("json");
+				logger.info("SdeDashBoard.groovy: ParamJson ="+json);
+				CreateWellParameter createWellParameter = CreateWellParameter.getFromJson(json);
+				result = SdeAccess.createWellList(createWellParameter);
+				logger.info("getWellList:Result="+result.toString());
+			}	
+			
+			else if("startSdeRequest".equals(action))
+			{
+				String json= request.getParameter("json");
+				logger.info("SdeDashBoard.groovy: ParamJson ="+json);
+				StartprocessParameter startprocessParameter = StartprocessParameter.getFromJson(json);
+				result = SdeAccess.startProcessParameter(startprocessParameter, session, processAPI, identityAPI );
+				logger.info("startProcessParameter:Result="+result.toString());
+			}
+			else if ("getListSystemSummary".equals(action))
+			{
+				String json= request.getParameter("json");
+				logger.info("SdeDashBoard.groovy: ParamJson ="+json);
+				SystemSummaryParameter systemSummaryParameter = SystemSummaryParameter.getFromJson(json);
+				result = SdeAccess.getListSystemSummary(systemSummaryParameter, session, processAPI );
+				logger.info("getListCasesForSdeDashboard:Result="+result.toString());
+			}	
+			
+			if (result!=null)
+			{
+				String jsonDetailsSt = JSONValue.toJSONString( result );
+	   
+				out.write( jsonDetailsSt );
+				out.flush();
+				out.close();				
+				return;				
+			}
+			logger.severe("Unknow command ["+action+"]");
+			
+			out.write( "Unknow command" );
+			out.flush();
+			out.close();
+			return;
+		} catch (Exception e) {
+			StringWriter sw = new StringWriter();
+			e.printStackTrace(new PrintWriter(sw));
+			String exceptionDetails = sw.toString();
+			logger.severe("SDEDashBoard.Groovy : Exception ["+e.toString()+"] at "+exceptionDetails);
+			
+			result = new HashMap<String,Object>();
+			
+			result.put("error", "Exception ["+e.toString()+"] at "+exceptionDetails);
+			String jsonDetailsSt = JSONValue.toJSONString( result );
+			out.write( jsonDetailsSt );
+			out.flush();
+			out.close();	
+			logger.severe("SDEDashBoard.Groovy : End flush json["+jsonDetailsSt+"]");
+			
+			return;				
+		}
+	}
+
+	
+	/** -------------------------------------------------------------------------
+	 *
+	 *runTheBonitaIndexDoGet
+	 * 
+	 */
+	private void runTheBonitaIndexDoGet(HttpServletRequest request, HttpServletResponse response, PageResourceProvider pageResourceProvider, PageContext pageContext) {
+				try {
+						def String indexContent;
+						pageResourceProvider.getResourceAsStream("index.html").withStream { InputStream s->
+								indexContent = s.getText()
+						}
+						
+						def String pageResource="pageResource?&page="+ request.getParameter("page")+"&location=";
+						
+						indexContent= indexContent.replace("@_USER_LOCALE_@", request.getParameter("locale"));
+						indexContent= indexContent.replace("@_PAGE_RESOURCE_@", pageResource);
+						
+						response.setCharacterEncoding("UTF-8");
+						PrintWriter out = response.getWriter();
+						out.print(indexContent);
+						out.flush();
+						out.close();
+				} catch (Exception e) {
+						e.printStackTrace();
+				}
+		}
+		
+		
+}
---
+++ custompage_SDEdashboard/resources/dashboardapp.js
@@ -0,0 +1,467 @@
+'use strict';
+/**
+ *
+ */
+
+(function() {
+
+
+var appCommand = angular.module('dashboardapp', ['googlechart', 'ui.bootstrap','ngModal']);
+
+
+
+
+
+// --------------------------------------------------------------------------
+//
+// Controler DashBoard
+//
+// --------------------------------------------------------------------------
+
+// DashBoard the server
+appCommand.controller('DashboardControler',
+	function ( $http, $scope, $filter ) {
+	
+	// this.listheader = [ {"title":"cardNo" },{ "title":"AccountNo"},{ "title":"CorporateAccount"},{ "title":"Customer Name"},{ "title":"Phone"} ];
+	this.listheader = [ {"title":"Date Well Identified", "id":"DateWellIdentified", "size":"12", "ctrl":"date"},
+						{"title":"UWI", "id":"WellCode" , "size":"20"}, 
+						{"title":"Well Full Name", "id":"WellFullName", "size":"20" }, 
+						{"title":"Business unit", "id":"BusinessUnit", "size":"12" }, 
+						{"title":"Request Type", "id":"RequestType", "size":"12" }, 
+						{"title":"Schd online Date", "id":"ScheduledOnlineDate","size":"20", "ctrl":"date" }, 
+						{"title":"Basic Well Data", "id":"BasicWellData", "size":"10", "ctrl":"semaphore" },
+						{"title":"Status", "id":"Status", "size":"20" }, 
+						{"title":"Assigned RO", "id":"AssignedRO", "size":"20" },
+						{"title":"", "id":"buttons", "size":"0","ctrl":"button" },
+						];
+
+	this.scheduledOnlineDateInFutur=true;
+	this.casemaxresult = 100;
+	this.listcases=[ ];
+	this.filtercase={};
+	this.sourceformurl="";
+	this.detailHistoryDataContributorEABU = { "sdenumber": "EABU",
+												"ScheduledOnlineDate": "",
+												"WellCode" : "Operations Geology Technical Assistant" ,
+												"WellFullName" : "Operations Geology Technical Assistant",
+												"FieldName" : "Operations Geology Technical Assistant",
+												"PermitSurface" : "GIS Officer",
+												"PermitBottomHole" : "GIS Officer",
+												"PadName" : "Operations Geology Technical Assistant",
+												"JointVentureName" : "Development Responsible Owner",
+												"SurfaceLatitude" : "Operations Geology Technical Assistant",
+												"SurfaceLongitude" : "Operations Geology Technical Assistant",
+												"BottomHoleLatitude" : "Operations Geology Technical Assistant",
+												"BottomHoleLongitude" : "Operations Geology Technical Assistant",
+												"CompletionType" : "Operations Geology Technical Assistant"
+												};
+	this.detailHistoryDataContributorGLNG = { 
+		"sdenumber": "GLNG",
+		"ScheduledOnlineDate": "",
+		"WellCode" : "TA subsurface gp - potentionally - tanya - Marivic will email",
+			"WellFullName" : "TA subsurface gp - potentionally - tanya - Marivic will email - see dbMap",
+			"WellAlias": "Field Development Planning Team Leader (for the relevant field)",
+			"FieldName" : "Production Data team",
+			"AreaNumber" : "Production Data team",
+			"WellCategoryPrimary": "",
+			"WellCategorySecondary1": "",
+			"WellCategorySecondary2": "",
+			"WellCategorySecondary3": "",
+			"PermitSurface" : "Field Development Planning Team Leader (for the relevant field)",
+			"PermitBottomHole" : "Field Development Planning Team Leader (for the relevant field)",
+			"PadName" : "Field Development Planning Team Leader (for the relevant field)",
+			"SurfaceLatitude" : "Field Development Planning Team Leader (for the relevant field)",
+			"SurfaceLongitude" : "Field Development Planning Team Leader (for the relevant field)",
+			"BottomHoleLatitude" : "Field Development Planning Team Leader (for the relevant field)",
+			"BottomHoleLongitude" : "Field Development Planning Team Leader (for the relevant field)",
+
+								};
+	this.detailHistoryDataContributor={};
+	this.isshowdetailcontributor=false;
+	
+	this.init = function() {
+		var self=this;
+	
+		$http.get( '../API/system/session/1').success( function (data ) {
+			self.userid = data.user_id; 
+			});
+	 };
+	this.init();
+	
+	this.refreshcases = function(  )
+	{
+	
+		var self=this;
+		this.filtercase={};
+		this.detailHistory={};
+		
+		this.orderByField='';
+		var post = { "maxresult" : this.casemaxresult, "scheduledOnlineDateInFutur":this.scheduledOnlineDateInFutur };
+		var json= angular.toJson(post, false);
+		console.log("Call Server to get list of cases");
+		$http.get( '?page=custompage_SDEdashboard&action=getWellTrackerDashboardList&json='+json )
+				.success( function ( jsonResult ) {
+
+					self.listcases 				= jsonResult.listcases;
+					console.log('get all task listBusinessFilter '+angular.toJson(self.listcases ));
+				})
+				.error( function(e) {
+					alert('an error occure during retrieve all cases');
+					console.log('error receive' , e);
+					});
+
+	};
+	this.refreshcases();
+
+
+	// --------------------------------------------------------------------------
+	//
+	//   Callback
+	//
+	// --------------------------------------------------------------------------
+	
+	this.link  =function( headerLabel, oneItem) 
+	{
+		
+		if (headerLabel.link == "accessBusinessUnit")
+			alert("call Businessunint "+oneItem.BusinessUnit);
+		// Url is 
+		// http://localhost:8080/bonita/portal/resource/processInstance/SDEdemo/1.0/content/?id=11003&locale=en
+
+		if (headerLabel.link == "accessDateWell") {
+			this.isshowform=true;
+			this.formtitle="Case "+oneItem.caseid;
+			this.sourceformurl= '/bonita/portal/resource/processInstance/'+oneItem.processname+'/'+oneItem.processversion+'/content/?id='+oneItem.caseid+'&typeOfDisplay=basicwellInfo';
+		}
+		if (headerLabel.link == "accessDateWellGLNV") {
+			this.isshowform=true;
+			this.formtitle="Case "+oneItem.caseid;
+			this.sourceformurl= '/bonita/portal/resource/processInstance/'+oneItem.processname+'/'+oneItem.processversion+'/content/?id='+oneItem.caseid+'&typeOfDisplay=basicwellGLNG';
+		}
+	}
+	
+	this.showDetails = function( oneTask ) {
+		this.showModal(); 
+		this.detailHistory = oneTask;
+		if (oneTask.BusinessUnit == "EABU")
+			this.detailHistoryDataContributor= this.detailHistoryDataContributorEABU;
+		else
+			this.detailHistoryDataContributor= this.detailHistoryDataContributorGLNG;
+	}
+	
+	this.errormessage="";
+	this.message ="";
+	this.initiateSdeRequestbutton = function( headerLabel, oneTask ) {
+	
+			var post = { "processvariables" : {"ctrSdeNumber_integer" : oneTask.sdenumber, "ctrSdeStatus_integer":oneTask.sdestatus},
+							"processname":"SDEdemo", 
+							"waitfirsttask":true, 
+							"timetowaitinms":"2000" };
+			var json= angular.toJson(post, false);
+			this.errormessage="";
+			this.message="Creation in progress";
+			
+
+			var self=this;
+			$http.get( '?page=custompage_SDEdashboard&action=startSdeRequest&json='+json )
+					.success( function ( jsonResult ) {
+						self.errormessage	= jsonResult.error;
+						
+						var processName			= jsonResult.processname;
+						var processVersion		= jsonResult.processversion;
+						var taskName			= jsonResult.taskName;
+						var caseId				= jsonResult.caseid;
+						var taskId				= jsonResult.taskid;
+						self.message			= jsonResult.message;
+						if (caseId!=null) {
+							self.message= self.message+"; Access the task...";
+							// assign the taskId
+							var assigndata = {"assigned_id" : self.userid  };
+							$http.put( '../API/bpm/humanTask/'+taskId )
+								.success( function ( jsonResult ) {		
+									self.message= "Display the task...";
+																		// alert("Case "+caseId+" is started - taskId="+taskId)
+									self.showtaskform( processName, processVersion, taskName, caseId, taskId);
+								})
+								.error( function ( data ) {
+									self.errormessage	= "The task can't be assign to you:" +angular.toJson(data);
+									self.message= "";
+									alert("the task can't be assign to you "+angular.toJson(data));
+								});
+						}
+						// self.isshowcases=false;
+					})
+					.error( function(e) {
+						self.errormessage	= "An error occured during create SDE Request";
+						self.message="";
+						alert('an error occure during create SDE Request');
+						console.log('error receive' , e);
+						});
+		}
+	this.accessTaskButton = function(headerLabel, oneTask ) {
+	
+		var processName			= oneTask.processname;
+		var processVersion		= oneTask.processversion;
+		var taskName			= oneTask.taskName;
+		var caseId				= oneTask.caseid;
+		var taskId				= oneTask.taskid;
+				
+		this.showtaskform( processName, processVersion, taskName, caseId, taskId);
+	}
+	
+	this.accessCaseOverviewButton = function(headerLabel, oneTask ) {
+	
+		var processName			= oneTask.processname;
+		var processVersion		= oneTask.processversion;
+		var taskName			= oneTask.taskName;
+		var caseId				= oneTask.caseid;
+		var taskId				= oneTask.taskid;
+		
+		this.formtitle = "Case "+caseId;
+		this.sourceformurl= '/bonita/portal/resource/processInstance/'+processName+'/'+processVersion+'/content/?id='+caseId;
+		console.log("URL: ",this.sourceformurl);
+		this.isshowcases=false;
+		this.isshowform=true;
+	}
+	// --------------------------------------------------------------------------
+	//
+	//   manage task force
+	//
+	// --------------------------------------------------------------------------
+	
+	this.showtaskform = function( processName, processVersion, taskName, caseId, taskId) {
+		this.formtitle = "Tasks "+caseId;
+		// this.sourceformurl= '/bonita/portal/homepage?ui=form&locale=en&tenant=1#form=' + processName + '--' + processVersion + '--' + taskName +'$entry&task=' + taskId+'&mode=form&assignTask=true'
+		// http://localhost:8080/bonita/portal/homepage#?id=20044&_p=performTask&_pf=1
+		this.sourceformurl= '/bonita/portal/resource/taskInstance/'+processName+'/'+processVersion+'/'+taskName+'/content/?id='+taskId;
+		console.log("URL: ",this.sourceformurl);
+		this.isshowcases=false;
+		this.isshowform=true;
+	};
+
+	this.showcases = function () {
+		this.isshowcases=true;
+		this.isshowform=false;
+	}
+	
+	// --------------------------------------------------------------------------
+	//
+	//   Modal page management
+	//
+	// --------------------------------------------------------------------------
+	this.dialogShown=false;
+	
+	this.showModal = function () {
+		this.dialogShown = !this.dialogShown;
+
+	}
+	
+	// --------------------------------------------------------------------------
+	//
+	//   Case page management
+	//
+	// --------------------------------------------------------------------------
+	this.casepagenumber=1;
+	this.filtercase = { };
+	this.caseitemsperpage=10;
+	// this function is call at each display : on a filter, or on a order for example
+	this.getCasesPage = function()
+	{
+		// console.log("getCasesPage : start "+this.listcases);
+		var begin = ((this.casepagenumber - 1) * this.caseitemsperpage);
+		var end = begin + this.caseitemsperpage;
+		this.listcases = $filter('orderBy')(this.listcases, this.orderByField, this.reverseSort);
+
+		var listcasesfiltered = $filter('filter') (this.listcases, this.filtercase );
+		// console.log('Filter filter='+ angular.toJson(this.filtercase,true ) +' Order='+  this.orderByField + ' reservesort='+this.reverseSort+' listcasesfiltered='+angular.toJson(listcasesfiltered));
+		return listcasesfiltered.slice(begin, end);
+	}
+
+
+
+	this.orderByField='';
+	this.reverseSort=true;
+	this.setorder= function( paramorderfield, paramreversesort)
+	{
+		this.orderByField = paramorderfield;
+		this.reverseSort = paramreversesort;
+		console.log("Order : ["+this.orderByField+"] order="+this.reverseSort);
+	}
+
+	// --------------------------------------------------------------------------
+	//
+	//   WellList page management
+	//
+	// --------------------------------------------------------------------------
+	this.updatesdedetails = {};
+	this.updatesdedetails.search={};
+	this.searchWellList = function() {
+		var self = this;
+		this.welllistmessage="Processing...";
+		this.welllisterrormessage="";
+
+		this.updatesdedetails.search.resultalist = true;
+		var json= angular.toJson(this.updatesdedetails.search, false);
+	
+		$http.get( '?page=custompage_SDEdashboard&action=getWellList&json='+json )
+			.then( function ( jsonResult ) {		
+						self.welllistmessage= jsonResult.data.DETAILS;
+						self.listwelllist = jsonResult.data.RESULT;
+						
+					},
+					function ( jsonResult ) {
+								self.welllisterrormessage	= "Error during get"+jsonResult.status;
+								self.message= "";
+								alert("Error during the request "+angular.toJson(jsonResult));
+							});
+	}
+	
+	
+	this.createWellList = function( onewell ) {
+		var self = this;
+		this.welllistmessage="Processing...";
+		this.welllisterrormessage="";
+		
+		var json= angular.toJson(onewell, false);
+	
+		$http.get( '?page=custompage_SDEdashboard&action=createWellList&json='+json )
+			.then( function ( jsonResult ) {		
+						self.welllistmessage= jsonResult.data.STATUS;
+						self.welllisterrormessage= jsonResult.data.ERRORSTATUS;
+					},
+					function ( jsonResult ) {
+								self.welllisterrormessage	= "Error during get"+jsonResult.status;
+								self.message= "";
+								alert("Error during the request "+angular.toJson(jsonResult));
+							});
+	}
+	
+	this.listwelllist=[];
+	this.welllistpagenumber=1;
+	this.filterwelllist = {};
+	this.welllistitemsperpage=10;
+	this.welllistorderByField="uwi";
+	this.welllistreverseSort=true;
+	// this function is call at each display : on a filter, or on a order for example
+	this.getWellListPage = function()
+	{
+		console.log("getWellList : start "+this.listwelllist);
+		if (this.listwelllist==null)
+		{
+			this.listwelllist=[];
+			return;
+		}
+		var begin = ((this.welllistpagenumber - 1) * this.welllistitemsperpage);
+		var end = begin + this.welllistitemsperpage;
+		this.listwelllist = $filter('orderBy')(this.listwelllist, this.welllistorderByField, this.welllistorderByField);
+
+		var listwelllistfiltered = $filter('filter') (this.listwelllist, this.filterwelllist );
+		console.log('Filter filter='+ angular.toJson( listwelllistfiltered,true ) +' Order='+  this.welllistorderByField + ' reservesort='+this.reverseSort+' listcasesfiltered='+angular.toJson(this.filterwelllist));
+		
+		return listwelllistfiltered.slice(begin, end);
+	}
+
+	// --------------------------------------------------------------------------
+	//
+	//   SystemSummary
+	//bob
+	// --------------------------------------------------------------------------
+
+	this.systemSummary={};
+	
+	this.systemSummary.search = {};
+	this.systemSummary.filter = {};
+	this.systemSummary.listsde=[];
+	this.systemSummary.pagenumber=1;
+	this.systemSummary.sdeperpage=50;
+	this.systemSummary.orderByField="SDE_NUMBER";
+	this.systemSummary.reversesort=true;
+	this.searchSystemSummary = function(  )
+	{
+		var self=this;
+		var json= angular.toJson(this.systemSummary.search, false);
+		console.log("Call Server to get list of cases");
+		$http.get( '?page=custompage_SDEdashboard&action=getListSystemSummary&json='+json )
+				.then( function ( jsonResult ) {
+					// console.log('searchSystemSummary: Sucess is '+angular.toJson(jsonResult.data ));
+					self.systemSummary.listsde 				= jsonResult.data.RESULT;
+					// console.log('searchSystemSummary: result is '+angular.toJson(self.systemSummary.listsde ));
+
+				}, function( jsonResult) {
+					alert('an error occure during retrieve all cases');
+					console.log('error receive' , e);
+				});
+
+	};
+	
+	// this function is call at each display : on a filter, or on a order for example
+	this.getSystemSummaryPage = function()
+	{
+		// console.log("---- getSystemSummaryPage : start list["+angular.toJson(this.systemSummary.listsde )+"]");
+		if (this.systemSummary.listsde==null)
+		{
+			this.systemSummary.listsde=[];
+			return this.systemSummary.listsde;
+		}
+		if (this.systemSummary.listsde.length ==0)
+		{
+			return this.systemSummary.listsde;
+		}		
+		var begin = ((this.systemSummary.pagenumber - 1) * this.systemSummary.sdeperpage);
+		var end = begin + this.systemSummary.sdeperpage;
+		this.systemSummary.listsde = $filter('orderBy')(this.systemSummary.listsde, this.systemSummary.orderByField, this.systemSummary.reversesort);
+		// console.log('-- getSystemSummaryPage orderBy='+ angular.toJson( this.systemSummary.listsde,true ));
+		
+		var listfiltered = $filter('filter') (this.systemSummary.listsde, this.systemSummary.filter );
+		console.log('-- getSystemSummaryPage filter='+angular.toJson(this.systemSummary.filter)+' listfiltered='+ angular.toJson( listfiltered,true ) +' Order='+  this.systemSummary.listsde.listsde + ' reservesort='+this.systemSummary.reversesort);
+		
+		return listfiltered.slice(begin, end);
+	}
+
+	// --------------------------------------------------------------------------
+	//
+	//   show pages
+	//
+	// --------------------------------------------------------------------------
+
+	this.isshowcases=true;
+	this.isshowform=false;
+	this.isshowSystemSummary=false;
+	this.isshowUpdateSdeDetails=false;
+	this.isshowPaDashboard=false;
+
+	this.resetView = function()
+	{
+		this.isshowcases=false;
+		this.isshowform=false;
+		this.isshowSystemSummary=false;
+		this.isshowUpdateSdeDetails=false;
+		this.isshowPaDashboard=false;
+		this.isshowdetailcontributor=false;
+	}
+	this.showViewForm = function() {
+		this.resetView();
+		this.isshowcases= true;
+		this.refreshcases();
+	};
+	this.showViewUpdateSde = function() {
+		this.resetView();
+		this.isshowUpdateSdeDetails= true;
+		
+	};
+	this.showViewSystemSummary = function() {
+		this.resetView();
+		this.isshowSystemSummary= true;
+	};
+	
+
+	this.getButtonClass = function (isCurrent ) {
+		if (isCurrent) 
+			return "btn btn-primary";
+		return "btn btn-success";
+	}
+});
+
+
+
+})();
